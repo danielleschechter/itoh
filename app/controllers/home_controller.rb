54077c33f84d3c6f30fc6f2eab8d775df574e63a
Danielle Schechter <danielle@ticketevolution.com>
2016-02-08 15:02:35 -0500

    profile images and tweet urls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @popular = Instagram.tag_recent_media('itstoughouthere')
   	twitter = TwitterClient.new(client_type: 'rest').setup_client
-	   @tweets = twitter.search("#love", result_type: "mixed").take(10)
+	   @tweets = twitter.search("#love", result_type: "mixed").take(2)
 	end
 end
 
