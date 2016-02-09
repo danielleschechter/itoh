d419b239d5953adc28bc7b5a629b2c58cd44e932
Danielle <danielleschechter@users.noreply.github.com>
2016-02-09 14:48:15 -0500

    Delete instagram.rb

--- config/initializers/instagram.rb
+++
@@ -1,8 +0,0 @@
-require "instagram"
-    Instagram.configure do |config|
-    config.client_id = "039e8b75e8d64f009134f501927940c3"
-    config.access_token = "13050785.039e8b7.df39d679ae414dfbb1dfc5d9e0e54bd2"
- end
-
-
- 
